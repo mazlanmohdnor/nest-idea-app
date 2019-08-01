7397637a06c3627bcfe836c5a9957eff0e803228
mazlanmohdnor <lan.psis@gmail.com>
2019-08-01 13:27:50 +0800

    added files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { IdeasModule } from './ideas/ideas.module';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), IdeasModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
